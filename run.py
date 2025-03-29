2f1160b4dd3c3311500394507b95dbb46329337f
Khaly DIENG <khaly.noflayetech@gmail.com>
2025-03-29 16:29:15 +0100

    add database initialization within app context

--- run.py
+++ run.py
@@ -4,5 +4,8 @@ from flask_migrate import Migrate
 app = create_app()
 migrate = Migrate(app, db)
 
+with app.app_context():
+    db.create_all()
+    
 if __name__ == '__main__':
     app.run(debug=True)
